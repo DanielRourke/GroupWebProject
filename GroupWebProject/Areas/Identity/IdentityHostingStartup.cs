ab3ecf082dc32f77c2e1eaa422d4e42f7fee6f34
Mark Hancock <hancockm910@gmail.com>
2020-09-09 19:35:23 +1000

    Authentication start

---
+++ GroupWebProject/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using GroupWebProject.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(GroupWebProject.Areas.Identity.IdentityHostingStartup))]
+namespace GroupWebProject.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
